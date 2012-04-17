a8d2e3c2b334ea56fd73c0507dcc82d2d3262341
Andrew Radev <andrey.radev@gmail.com>
2012-04-17 20:15:15 +0300

    Get binary working

--- lib/vimrunner.rb
+++ lib/vimrunner.rb
@@ -1,1 +1,12 @@
-require 'vimrunner/runner'
+require 'vimrunner/client'
+require 'vimrunner/server'
+
+module Vimrunner
+  def self.start_vim
+    Client.new(Server.start)
+  end
+
+  def self.start_gui_vim
+    Client.new(Server.start(:gui => true))
+  end
+end
