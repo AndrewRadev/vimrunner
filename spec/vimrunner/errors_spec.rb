a5e47e3e333e562a0b75275465e4299394397c6f
Andrew Radev <andrey.radev@gmail.com>
2012-10-30 19:57:53 +0200

    Add a spec for InvalidCommandError

--- spec/vimrunner/errors_spec.rb
+++ spec/vimrunner/errors_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "vimrunner"
 require "vimrunner/errors"
 
 module Vimrunner
@@ -6,7 +7,7 @@ module Vimrunner
     it "has a useful message" do
       expect {
         raise NoSuitableVimError
-      }.to raise_error("No suitable Vim executable could be found for this system.")
+      }.to raise_error(/No suitable Vim executable could be found for this system./)
     end
   end
 
@@ -14,7 +15,17 @@ module Vimrunner
     it "has a useful message" do
       expect {
         raise TimeoutError
-      }.to raise_error("Timed out while waiting for serverlist. Is an X11 server running?")
+      }.to raise_error(/Timed out while waiting for serverlist. Is an X11 server running?/)
+    end
+  end
+
+  describe InvalidCommandError do
+    it "has a useful message" do
+      expect {
+        Vimrunner.start do |vim|
+          vim.command :nonexistent
+        end
+      }.to raise_error(/Not an editor command: nonexistent/)
     end
   end
 end
